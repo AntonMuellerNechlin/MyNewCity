2d7857d73689b42eb73e77dfb18d84e498368ef2
Anton <antonmuellernechlin@web.de>
2026-05-18 14:33:41 +0200

    Initial commit

---
+++ app/src/main/java/MainActivity.kt
@@ -0,0 +1,43 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var map: MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        Configuration.getInstance().load(
+            applicationContext,
+            getSharedPreferences("osmdroid", MODE_PRIVATE)
+        )
+        Configuration.getInstance().userAgentValue = packageName
+
+        setContentView(R.layout.activity_main)
+
+        map = findViewById(R.id.map)
+
+        map.setMultiTouchControls(true)
+
+        val controller = map.controller
+        controller.setZoom(15.0)
+
+        val startPoint = GeoPoint(52.5200, 13.4050) // Berlin
+        controller.setCenter(startPoint)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map.onPause()
+    }
+}
